1049f8cbdcfe1db3849a7784195fece16a51c5a0
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-04-07 19:05:59 -0300

    feat: front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./logo.svg"
 
 function App() {
+  const usuario = "Beggiana"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parabens, <br/> <span>{usuario}</span>.</h1>
+      <h2>Voce foi</h2>
+      <h1><span>aprovado</span></h1>
+      <h2>no processo seletivo!</h2>
+
+      <img src={logo} alt="logotipo echosec" />
+
+      <h3>Agora voce tem um encontro marcado conosco toda <span>terca-feira</span>, das <span>13h as 14h</span>.</h3>
+
+      <p>Mas, antes disso, voce precisa confirmar a sua <a href="https://forms.gle/EQf8dSFwgM4XnNF79">inscricao</a> no clube e conhecer a nossa <a href="https://www.canva.com/design/DAGi-sfQylE/LGKWhFjHWvpDA74CrB6kuA/edit?utm_content=DAGi-sfQylE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" >estrutura organizacional</a>.</p>
     </div>
   );
 }
