3d302acb0d97b42552e43c5fa3e31d88f5123af4
RaphaelSilva09 <raphael.silva@sou.inteli.edu.br>
2025-04-09 17:28:23 -0300

    fix: correção das rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Aprovacao from './pages/aprovado';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/:id' element={ <Aprovacao /> } />
-        </Routes>
-      </Router>
-    </div>
+    <Router basename="/landing-aprovados2025">
+      <Routes>
+        <Route path="/:id" element={<Aprovacao />} />
+      </Routes>
+    </Router>
   );
 }
 
